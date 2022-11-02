58b701c51f21f73f558bd0996979a2dafbe8bf6b
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2022-11-02 08:40:35 -0300

    Inserting a User with POST

--- src/main/java/com/thegaprado/workshopmongo/services/UserService.java
+++ src/main/java/com/thegaprado/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thegaprado.workshopmongo.domain.User;
+import com.thegaprado.workshopmongo.dto.UserDTO;
 import com.thegaprado.workshopmongo.repository.UserRepository;
 import com.thegaprado.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
